b6dcd08d589db00df9951ebc51ca35fb0aca30c6
Zhrhashemian <116919442+Zhrhashemian@users.noreply.github.com>
2022-10-29 19:26:59 +0330

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,12 @@
+using System;
+class Boolean
+{
+	public static void Main(string[] args)
+	{
+		bool content = true;
+		bool nocontent = false;
+		Console.WriteLine("text is {0}",content);
+		Console.WriteLine("text is {0}",nocontent);
+	}
+
+}
