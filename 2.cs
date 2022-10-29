843716ebe6e11596e54ca90c2ad48f069b1c1f03
Zhrhashemian <116919442+Zhrhashemian@users.noreply.github.com>
2022-10-29 19:49:00 +0330

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,13 @@
+using  System;
+namespace consoleApp4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("text is{0}",args[0]);
+           
+        }
+    }
+}
